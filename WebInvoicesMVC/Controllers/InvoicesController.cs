2f45f8a5de168dbdf85d8bb388899484efcdc063
cjbg <cjbg@o2.pl>
2017-10-17 16:09:15 +0200

    Refresh Views, refresh controller Invoices.

--- WebInvoicesMVC/Controllers/InvoicesController.cs
+++ WebInvoicesMVC/Controllers/InvoicesController.cs
@@ -6,119 +6,124 @@ using Invoices.Entities;
 using WebInvoicesMVC.DataContexts;
 
 namespace WebInvoicesMVC.Controllers
-{  
-  public class InvoicesController : Controller
-  {
-    private InvoicesDb db = new InvoicesDb();
-
-    // GET: Invoices
-    public ActionResult Index()
+{
+    public class InvoicesController : Controller
     {
-      return View(db.Invoices.ToList());
-    }
+        private InvoicesDb db = new InvoicesDb();
 
-    // GET: Invoices/Details/5
-    public ActionResult Details(int? id)
-    {
-      if (id == null)
-      {
-        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-      }
-      Invoice invoice = db.Invoices.Find(id);
-      if (invoice == null)
-      {
-        return HttpNotFound();
-      }
-      return View(invoice);
-    }
+        // GET: Invoices
+        public ActionResult Index()
+        {
+            var invoices = db.Invoices.Include(i => i.Client);
+            return View(invoices.ToList());
+        }
 
-    // GET: Invoices/Create
-    public ActionResult Create()
-    {
-      return View();
-    }
+        // GET: Invoices/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Invoice invoice = db.Invoices.Find(id);
+            if (invoice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(invoice);
+        }
 
-    // POST: Invoices/Create
-    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Create([Bind(Include = "Id,Name,Ammount,Currency")] Invoice invoice)
-    {
-      if (ModelState.IsValid)
-      {
-        db.Invoices.Add(invoice);
-        db.SaveChanges();
-        return RedirectToAction("Index");
-      }
+        // GET: Invoices/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "Name");
+            return View();
+        }
 
-      return View(invoice);
-    }
+        // POST: Invoices/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Currency,ClientId")] Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Invoices.Add(invoice);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-    // GET: Invoices/Edit/5
-    public ActionResult Edit(int? id)
-    {
-      if (id == null)
-      {
-        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-      }
-      Invoice invoice = db.Invoices.Find(id);
-      if (invoice == null)
-      {
-        return HttpNotFound();
-      }
-      return View(invoice);
-    }
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "Name", invoice.ClientId);
+            return View(invoice);
+        }
 
-    // POST: Invoices/Edit/5
-    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Edit([Bind(Include = "Id,Name,Ammount,Currency")] Invoice invoice)
-    {
-      if (ModelState.IsValid)
-      {
-        db.Entry(invoice).State = EntityState.Modified;
-        db.SaveChanges();
-        return RedirectToAction("Index");
-      }
-      return View(invoice);
-    }
+        // GET: Invoices/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Invoice invoice = db.Invoices.Find(id);
+            if (invoice == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "Name", invoice.ClientId);
+            return View(invoice);
+        }
 
-    // GET: Invoices/Delete/5
-    public ActionResult Delete(int? id)
-    {
-      if (id == null)
-      {
-        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-      }
-      Invoice invoice = db.Invoices.Find(id);
-      if (invoice == null)
-      {
-        return HttpNotFound();
-      }
-      return View(invoice);
-    }
+        // POST: Invoices/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Currency,ClientId")] Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(invoice).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClientId = new SelectList(db.Clients, "Id", "Name", invoice.ClientId);
+            return View(invoice);
+        }
 
-    // POST: Invoices/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public ActionResult DeleteConfirmed(int id)
-    {
-      Invoice invoice = db.Invoices.Find(id);
-      db.Invoices.Remove(invoice);
-      db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+        // GET: Invoices/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Invoice invoice = db.Invoices.Find(id);
+            if (invoice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(invoice);
+        }
 
-    protected override void Dispose(bool disposing)
-    {
-      if (disposing)
-      {
-        db.Dispose();
-      }
-      base.Dispose(disposing);
+        // POST: Invoices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Invoice invoice = db.Invoices.Find(id);
+            db.Invoices.Remove(invoice);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
-  }
 }
